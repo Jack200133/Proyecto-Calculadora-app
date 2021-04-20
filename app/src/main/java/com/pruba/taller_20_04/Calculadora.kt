8f16d5ab285da8ef2cf9366b35a423d096a22904
Jack200133 <car20593@uvg.edu.gt>
2021-04-20 13:02:40 -0600

    Todo lo grafico terminado

--- app/src/main/java/com/pruba/taller_20_04/Calculadora.kt
+++ app/src/main/java/com/pruba/taller_20_04/Calculadora.kt
@@ -2,15 +2,36 @@ package com.pruba.taller_20_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class Calculadora : AppCompatActivity() {
     lateinit var txtResultado: TextView
+    lateinit var btnUno: Button
+    lateinit var btnDos: Button
+    lateinit var btnTres: Button
+    lateinit var btnMas: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora)
 
         txtResultado = findViewById(R.id.textResultado)
+
+        btnUno = findViewById(R.id.btnUno)
+        btnDos = findViewById(R.id.btnDos)
+        btnTres = findViewById(R.id.btnTres)
+        btnMas = findViewById(R.id.btnMas)
+
+        btnUno.setOnClickListener{
+            txtResultado.text = "1"
+        }
+
+        btnDos.setOnClickListener{
+            txtResultado.text = "2"
+        }
+        btnTres.setOnClickListener{
+            txtResultado.text = "3"
+        }
     }
 }
