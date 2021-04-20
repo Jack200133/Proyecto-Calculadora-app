fec8c030dd093ae300fb346afedfdce17c7164dd
Jack200133 <car20593@uvg.edu.gt>
2021-04-20 12:28:41 -0600

    Parte Grafica de la calculadora

--- app/src/main/java/com/pruba/taller_20_04/Calculadora.kt
+++ app/src/main/java/com/pruba/taller_20_04/Calculadora.kt
@@ -2,10 +2,15 @@ package com.pruba.taller_20_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Calculadora : AppCompatActivity() {
+    lateinit var txtResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora)
+
+        txtResultado = findViewById(R.id.textResultado)
     }
 }
