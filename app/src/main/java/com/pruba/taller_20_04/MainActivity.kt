6503bc640fd26bd4228836d8f0f4b07ac742ee0d
Jack200133 <car20593@uvg.edu.gt>
2021-04-20 11:15:23 -0600

    Creacion de taller y toast

---
+++ app/src/main/java/com/pruba/taller_20_04/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.pruba.taller_20_04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"onCreate", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+}
